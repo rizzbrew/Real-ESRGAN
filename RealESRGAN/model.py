a56592c4add56178cb4ebea996aab903b5716e74
RizzyFuzz <191101374+rizzbrew@users.noreply.github.com>
2025-05-30 00:54:16 +0800

    Update Huggingface Issues

--- RealESRGAN/model.py
+++ RealESRGAN/model.py
@@ -4,12 +4,11 @@ from torch.nn import functional as F
 from PIL import Image
 import numpy as np
 import cv2
-from huggingface_hub import hf_hub_url, cached_download
+from huggingface_hub import hf_hub_download  # GANTI INI
 
 from .rrdbnet_arch import RRDBNet
 from .utils import pad_reflect, split_image_into_overlapping_patches, stich_together, \
                    unpad_image
-
 
 HF_MODELS = {
     2: dict(
@@ -26,7 +25,6 @@ HF_MODELS = {
     ),
 }
 
-
 class RealESRGAN:
     def __init__(self, device, scale=4):
         self.device = device
@@ -38,13 +36,21 @@ class RealESRGAN:
         
     def load_weights(self, model_path, download=True):
         if not os.path.exists(model_path) and download:
-            assert self.scale in [2,4,8], 'You can download models only with scales: 2, 4, 8'
+            assert self.scale in [2, 4, 8], 'You can download models only with scales: 2, 4, 8'
             config = HF_MODELS[self.scale]
-            cache_dir = os.path.dirname(model_path)
             local_filename = os.path.basename(model_path)
-            config_file_url = hf_hub_url(repo_id=config['repo_id'], filename=config['filename'])
-            cached_download(config_file_url, cache_dir=cache_dir, force_filename=local_filename)
-            print('Weights downloaded to:', os.path.join(cache_dir, local_filename))
+
+            # GANTI INI DENGAN hf_hub_download
+            downloaded_path = hf_hub_download(
+                repo_id=config['repo_id'],
+                filename=config['filename'],
+                cache_dir=os.path.dirname(model_path),
+                local_dir_use_symlinks=False
+            )
+
+            # SALIN KE LOKASI YANG DITENTUKAN
+            os.rename(downloaded_path, model_path)
+            print('Weights downloaded to:', model_path)
         
         loadnet = torch.load(model_path)
         if 'params' in loadnet:
@@ -55,8 +61,9 @@ class RealESRGAN:
             self.model.load_state_dict(loadnet, strict=True)
         self.model.eval()
         self.model.to(self.device)
-        
-    @torch.cuda.amp.autocast()
+
+    # PERINGATAN: GANTI DEKORATOR AMP
+    @torch.amp.autocast(device_type='cuda')
     def predict(self, lr_image, batch_size=4, patches_size=192,
                 padding=24, pad_size=15):
         scale = self.scale
